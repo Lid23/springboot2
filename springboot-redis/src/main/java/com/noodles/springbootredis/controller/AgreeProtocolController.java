f1f8198727428a5c39a9e60e55499f58923348d6
Lid23 <645465550@qq.com>
2020-04-09 18:29:06 +0800

    20200409

---
+++ springboot-redis/src/main/java/com/noodles/springbootredis/controller/AgreeProtocolController.java
@@ -0,0 +1,17 @@
+package com.noodles.springbootredis.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @filename AgreeProtocolController
+ * @description 使用某功能时，如果是首次进入会展示一个协议页面，用户需要勾选后点确定才能进入功能，
+ * 此后再进该功能，不再显示协议页直接进入该功能
+ * 头条链接：https://www.toutiao.com/i6765827466045948427/?tt_from=weixin&utm_campaign=client_share&wxshare_count=1&timestamp=1586341110&app=news_article&utm_source=weixin&utm_medium=toutiao_android&req_id=202004081818290100260600752713E992&group_id=6765827466045948427
+ * @author 巫威
+ * @date 2020/4/8 18:44
+ */
+
+@RestController
+public class AgreeProtocolController {
+
+}
